82d0f5c940cca14675efc3e0c0e2bcbfab8dc214
Rainer Minixhofer <rainer.minixhofer@dialogus.at>
2020-03-28 23:06:13 +0100

    Convert distance into fill-height and return fill-height

--- range_sensor.py
+++ range_sensor.py
@@ -9,6 +9,7 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 10
 ECHO = 4
 ISEID = 25608
+CISTERNHEIGHT = 250
 logging.basicConfig(level=logging.INFO, \
 	filename='/var/log/rangesensor.log', \
 	filemode='a', format = "%(asctime)s: %(name)s - %(levelname)s - %(message)s")
@@ -37,9 +38,10 @@ pulse_duration = pulse_end - pulse_start
 distance = pulse_duration * 17150
 
 distance = round(distance, 2)
+height = CISTERNHEIGHT - distance
 
 #write datum into homematic
-params = {'ise_id': ISEID, 'new_value': distance}
+params = {'ise_id': ISEID, 'new_value': height}
 URL = 'http://homematic.minixint.at/config/xmlapi/statechange.cgi'
 
 try:
@@ -48,5 +50,6 @@ except requests.exceptions.RequestException as e:
     logging.error("Error occured, trying again later: ", exc_info=err)
 
 logging.info("Distance: %6.2f cm", distance)
+logging.info("Height: %6.2f cm", height)
 
 GPIO.cleanup()
