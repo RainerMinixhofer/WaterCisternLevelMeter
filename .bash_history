44ce5965632c2fa59f3eb775c032e10119a02be6
Rainer Minixhofer <rainer.minixhofer@dialogus.at>
2020-03-28 23:02:22 +0100

    Initial commit

---
+++ .bash_history
@@ -0,0 +1,67 @@
+ls
+sudo raspi-config
+sudo userdel -r pi
+ps aux
+sudo kill -9 528
+ps aux
+sudo kill -9 538
+sudo kill -9 541
+ps aux
+sudo userdel -r pi
+sudo apt-get update -y
+sudo apt-get upgrade -y
+cd
+ls
+more range_sensor.py 
+ls -al
+./range_sensor.py 
+python ./range_sensor.py 
+sudo python ./range_sensor.py 
+exit
+df -k
+uname -a
+ls
+pwd
+more range_sensor.py 
+ls
+more range_sensor.py 
+python3 range_sensor.py 
+python range_sensor.py 
+sudo python range_sensor.py 
+exit
+ls
+timedatectl
+sudo nano /etc/systemd/timesyncd.conf 
+sudo timedatectl set-ntp 0
+sudo timedatectl set-ntp 1
+
+sudo timedatectl set-ntp 1
+timedatectl
+sudo dpkg-reconfigure tzdata
+timedatectl
+sudo apt install fake-hwclock
+exit
+sudo shutdown -h now
+pwd
+ls
+sudo python range_sensor.py 
+exit
+sudo python range_sensor.py 
+exit
+sudo python range_sensor.py 
+pwd
+ls
+cat range_sensor.py 
+l
+cat /etc/group
+cat /etc/passwd
+groups rainer
+sudo adduser rainer gpio
+ls
+python range_sensor.py 
+id
+exit
+id
+ls
+python range_sensor.py 
+exit
---
+++ range_sensor.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import requests
+import logging
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 10
+ECHO = 4
+ISEID = 25608
+logging.basicConfig(level=logging.INFO, \
+	filename='/var/log/rangesensor.log', \
+	filemode='a', format = "%(asctime)s: %(name)s - %(levelname)s - %(message)s")
+
+logging.info("Distance Measurement In Progress")
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+logging.debug("Waiting For Sensor To Settle")
+time.sleep(2)
+
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO)==0:
+  pulse_start = time.time()
+
+while GPIO.input(ECHO)==1:
+  pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start
+
+distance = pulse_duration * 17150
+
+distance = round(distance, 2)
+
+#write datum into homematic
+params = {'ise_id': ISEID, 'new_value': distance}
+URL = 'http://homematic.minixint.at/config/xmlapi/statechange.cgi'
+
+try:
+    result = requests.get(URL, params=params)
+except requests.exceptions.RequestException as e:
+    logging.error("Error occured, trying again later: ", exc_info=err)
+
+logging.info("Distance: %6.2f cm", distance)
+
+GPIO.cleanup()
