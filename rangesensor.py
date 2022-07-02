6c9594c8d120cbb58058df1563b6e32ed08aaefd
Rainer Minixhofer <rainer.minixhofer@dialogus.at>
2022-07-02 19:02:01 +0200

    Fixed pylint errors. introduced 3ms minimum and 38ms maximum pulse time to prevent too short measurements

--- rangesensor.py
+++ rangesensor.py
@@ -9,7 +9,7 @@ Created on 10.04.2020
 
 @author: Rainer Minixhofer
 """
-#pylint: disable=C0103, C0301, W0603, R0903, R1722
+#pylint: disable=C0103, C0209, C0301, W0603, W0703, R0903, R1722
 import os
 import re
 import signal
@@ -22,8 +22,8 @@ import requests
 import numpy as np
 import pigpio #pylint: disable=E0401
 import wiringpi #pylint: disable=E0401
-import soundspeed
 from gpiozero import CPUTemperature #pylint: disable=E0401
+import soundspeed
 
 
 #Parse command line
@@ -48,13 +48,6 @@ parser.add_argument('--averaging',
                           taken and outliers are skipped which are deviating more \
                           than 6*sigma from this median. The remaining measurements are \
                           averaged for the result. (Default 50)')
-# Argument to specify location of ASI SDK Library (default specified in env_filename
-parser.add_argument('--GPIOlib',
-                    nargs='?',
-                    const='PiGPIO',
-                    default='PiGPIO',
-                    help='GPIO library to be used. Currently WiringPi and PiGPIO are \
-                        supported. (Default PiGPIO)')
 # If specified the logging messages are output to stdout as well
 parser.add_argument('--stdoutlog',
                     action='store_true',
@@ -85,14 +78,6 @@ if args.stdoutlog:
     logging.getLogger().addHandler(logging.StreamHandler())
 
 logging.info("Daemon RangeSensor started.")
-
-#Check consistency of command line arguments
-
-if args.GPIOlib.lower() not in ['wiringpi', 'pigpio']:
-    logging.error("Only WiringPI and PiGPIO as GPIO libraries supported.")
-    exit()
-else:
-    usewiringpi = (args.GPIOlib.lower() == 'wiringpi')
 
 if args.averaging*0.05 > args.measureinterval > 0:
     logging.error("Total averaging time takes longer than interval between measurements.")
@@ -127,11 +112,7 @@ class GracefulKiller:
 
 killer = GracefulKiller()
 
-if usewiringpi:
-#    GPIO.setmode(GPIO.BCM)
-    wiringpi.wiringPiSetupGpio()
-else:
-    pi = pigpio.pi()
+pi = pigpio.pi()
 
 #URL for saving measured parameters into
 URL = 'http://homematic.minix.local/config/xmlapi/statechange.cgi'
@@ -164,17 +145,9 @@ WATERFALLRATE = QPUMP*1000/CISTERNAREA # Maximum rate of sinking waterlevel chan
 pulse_start = None
 pulse_end = None
 
-if usewiringpi:
-    wiringpi.pinMode(TRIG, wiringpi.GPIO.INPUT)
-    wiringpi.pinMode(ECHO, wiringpi.GPIO.OUTPUT)
-    wiringpi.pullUpDnControl(ECHO, wiringpi.GPIO.PUD_OFF)
-#    GPIO.setup(TRIG, GPIO.OUT)
-#    GPIO.setup(ECHO, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-#    GPIO.remove_event_detect(ECHO)
-else:
-    pi.set_mode(TRIG, pigpio.OUTPUT)
-    pi.set_mode(ECHO, pigpio.INPUT)
-    pi.set_pull_up_down(ECHO, pigpio.PUD_OFF)
+pi.set_mode(TRIG, pigpio.OUTPUT)
+pi.set_mode(ECHO, pigpio.INPUT)
+pi.set_pull_up_down(ECHO, pigpio.PUD_OFF)
 
 # Open datafile for processing
 
@@ -185,7 +158,7 @@ datfile = workdir+"/rangesensor.dat"
 # Check if first datapoint in file is in previous month. If this is the case
 # Archive the datafile and remove datafile to generate new one
 if os.path.isfile(datfile):
-    with open(datfile) as f:
+    with open(datfile, encoding="utf8") as f:
         f.readline()
         firstdatetime = datetime.strptime(f.readline().split(',')[0], fdatetime)
     curdatetime = datetime.now()
@@ -197,13 +170,13 @@ if os.path.isfile(datfile):
         os.remove(datfile)
 #Read in datafile if exists otherwise open new one
 if os.path.isfile(datfile):
-    f = open(datfile, "a+", buffering=1) #pylint: disable=R1732
+    f = open(datfile, "a+", buffering=1, encoding="utf8") #pylint: disable=R1732
     logging.info('Reading previous data from datafile %s', datfile)
     databuffer = np.genfromtxt(datfile, delimiter=',', invalid_raise=False, skip_header=1)
     logging.info('Read %d lines', databuffer.shape[0])
 
 else:
-    f = open(datfile, "w", buffering=1) #pylint: disable=R1732
+    f = open(datfile, "w", buffering=1, encoding="utf8") #pylint: disable=R1732
     #Write Header when opened the first time
     f.write("DateTime,Distance,Height,Stored_Water,Fill_Height,CPU_Temp,Air_Temp\n")
     databuffer = np.array([])
@@ -222,15 +195,15 @@ def measure_temperature(devid):
         Temperature in degrees Celsius.
 
     """
+    tstring = "20000"
     try:
-        with open("/sys/bus/w1/devices/"+devid+"/w1_slave", "r") as devf:
+        with open("/sys/bus/w1/devices/"+devid+"/w1_slave", "r", encoding="utf8") as devf:
             devcont = devf.read()
             tstring = re.search(r"t=(\d+)\n", devcont).group(1)
-    except (IOError, ValueError, EOFError) as e:
-        print(e)
-        tstring = '20000'
-    except: #pylint: disable=W0702
-        print('Unknown Error')
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
     return int(tstring)/1000
 
 def measure_flank_time_pigpio(pin, level, tick):
@@ -262,17 +235,9 @@ def measure_flank_time_wiringpi():
         pulse_end = wiringpi.micros()
     return True
 
-if usewiringpi:
-    if not args.staticdetect:
-        wiringpi.wiringPiISR(ECHO, wiringpi.GPIO.INT_EDGE_BOTH, \
-                             measure_flank_time_wiringpi)
-#        GPIO.add_event_detect(ECHO, GPIO.BOTH, callback=measure_flank_time_wiringpi)
-    wiringpi.digitalWrite(TRIG, 0)
-#    GPIO.output(TRIG, False)
-else:
-    if not args.staticdetect:
-        pi.callback(ECHO, 2, measure_flank_time_pigpio)
-    pi.write(TRIG, False)
+if not args.staticdetect:
+    pi.callback(ECHO, 2, measure_flank_time_pigpio)
+pi.write(TRIG, False)
 
 logging.info("Distance measurement daemon RangeSensor started. Waiting for sensor to settle")
 time.sleep(2)
@@ -294,15 +259,7 @@ while not killer.kill_now:
         #set trigger for 10us to high. The ultrasonic signal (8x40kHz bursts)
         #is sent out with the falling flank #of the TRIG output.
         logging.debug("Send Trigger pulse on TRIG pin")
-        if usewiringpi:
-            wiringpi.digitalWrite(TRIG, 1)
-    #        GPIO.output(TRIG, True)
-            wiringpi.delayMicroseconds(20) # According to the web sensor has problems with 10us Trigger pulse width, 20us is more stable
-    #        time.sleep(0.00001) # Not very accurate. Need better solution
-            wiringpi.digitalWrite(TRIG, 0)
-    #        GPIO.output(TRIG, False)
-        else:
-            pi.gpio_trigger(TRIG, 10, 1)
+        pi.gpio_trigger(TRIG, 10, 1)
 
         #After the burst is sent the ECHO pin is going from low to high and
         #stays high until the echo of the bursts is detected. Thus the duration
@@ -313,19 +270,17 @@ while not killer.kill_now:
         #until the flank high/low occurs (when args.staticdetect is True).
         logging.debug("Iteration %d: Waiting for response", idx[0]+1)
         if args.staticdetect:
-            while usewiringpi and wiringpi.digitalRead(ECHO) == 0 or not usewiringpi and pi.read(ECHO) == 0:
+            while pi.read(ECHO) == 0:
                 pulse_start = time.time()
-            while usewiringpi and wiringpi.digitalRead(ECHO) == 1 or not usewiringpi and pi.read(ECHO) == 1:
+            while pi.read(ECHO) == 1:
                 pulse_end = time.time()
             #Convert into Microseconds timing
+#            import pdb; pdb.set_trace()
             pulse_end = (pulse_end - pulse_start)*10**6
             pulse_start = 0
         else:
             #Wait a bit longer than maximum allowed time of high signal on ECHO pin.
-            if usewiringpi:
-                wiringpi.delay(40)
-            else:
-                time.sleep(0.040)
+            time.sleep(0.040)
         #The time ticks are wrapping around at the max value of an unsigned 32-bit number thus we take the modulo
         logging.debug("Detected rising edge at time-stamp %d", pulse_start)
         logging.debug("Detected falling edge at time-stamp %d", pulse_end)
@@ -334,10 +289,11 @@ while not killer.kill_now:
         logging.debug("ECHO Pulse duration %.1f us", pulse_duration)
 
     # Drop all measurements which deviate more than <OUTLIERSIGMAFACT>xStandardDeviation from Median value
+    # Furthermore drop all measurements which are longer than 38ms or are shorter than 3ms
     print(arr)
     pd_median = np.median(arr)
     pd_stddev = OUTLIERSIGMAFACT * np.std(arr)
-    selector = ((pd_median - pd_stddev) < arr) & (arr < (pd_median + pd_stddev))
+    selector = ((pd_median - pd_stddev) < arr) & (arr < (pd_median + pd_stddev)) & (arr > 3000) & (arr < 38000)
     nroutliers = np.count_nonzero(np.invert(selector))
     if nroutliers > 0:
         logging.info("We have %d outlier(s) which have been dropped from averaging.", nroutliers)
@@ -363,7 +319,7 @@ while not killer.kill_now:
     # Outlier Check
 
     #write datum into homematic
-    if 0 <= filling <= 100:
+    if 0 <= filling < 110:
 
         try:
             result = requests.get(URL + '?ise_id=%d,%d,%d' % (HEIGHTISEID, WATERISEID, FILLINGISEID) + \
@@ -386,8 +342,8 @@ while not killer.kill_now:
         logging.info("Fill Height: %.2f %%", filling)
 
     else:
-        logging.error("Measured fill height out of bounds (0%%<%.2f<100%%)", filling)
-        logging.info("Pulse Duration: %6.2f us", pulse_duration*10**6)
+        logging.error("Measured fill height out of bounds (0%%<%.2f<110%%)", filling)
+        logging.info("Pulse Duration: %6.2f us", pulse_duration)
         logging.info("Distance: %6.2f cm", distance)
 
     f.write("%s,%6.2f,%6.2f,%5d,%.2f" % (date_time.strftime(fdatetime), distance, height, water, filling))
@@ -412,6 +368,5 @@ while not killer.kill_now:
         killer.kill_now = True
 
 f.close()
-if not usewiringpi:
-    pi.stop()
+pi.stop()
 logging.info("Daemon RangeSensor stopped.")
